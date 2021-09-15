98f10ea17c874d5b09d220582176f45446eae707
foxthere <weidufox@gmail.com>
2021-09-15 15:45:43 +0300

    增加 基于XC7Z020clg400-2的PS端KEY控制LED实验

---
+++ XC7Z020_205_ES_key_control_led_GPIO_MIO/XC7Z020_205_ES_key_control_led_GPIO_MIO.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_key_control_led_gpio_emio.elf_on_local.tcl
@@ -0,0 +1,14 @@
+connect -url tcp:127.0.0.1:3121
+source C:/Users/mi/Desktop/Programma/ZYNQ/XC7Z020_205_ES_key_control_led_GPIO_MIO/XC7Z020_205_ES_key_control_led_GPIO_MIO.sdk/ps_design_wrapper_hw_platform_0/ps7_init.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-HS2 201706300081"} -index 0
+loadhw C:/Users/mi/Desktop/Programma/ZYNQ/XC7Z020_205_ES_key_control_led_GPIO_MIO/XC7Z020_205_ES_key_control_led_GPIO_MIO.sdk/ps_design_wrapper_hw_platform_0/system.hdf
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-HS2 201706300081"} -index 0
+stop
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent JTAG-HS2 201706300081"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent JTAG-HS2 201706300081"} -index 0
+dow C:/Users/mi/Desktop/Programma/ZYNQ/XC7Z020_205_ES_key_control_led_GPIO_MIO/XC7Z020_205_ES_key_control_led_GPIO_MIO.sdk/key_control_led_GPIO_EMIO/Debug/key_control_led_GPIO_EMIO.elf
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent JTAG-HS2 201706300081"} -index 0
+con
